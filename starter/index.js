e0fb8f1f1355c1302baa58ace3b168251ef1ba37
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-24 20:16:37 +0000

    manager func defined

--- starter/index.js
+++ starter/index.js
@@ -12,69 +12,69 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-let theManager = {};
-
-
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your name?',
-            name: 'name',
-            validate(name) {
-                if(!/[a-zA-Z]+$/.test(name)) {
-                    return "Please only use letters";
-                }
-                return true
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your emplyee ID?',
-            name: 'id',
-            validate(id) {
-                if(/[a-zA-Z]+$/.test(id)) {
-                    return "Please only use numbers";
-                }
-                return true
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email',
-            validate(email) {
-                //simple validation, as emails are super complicated to validate
-                if(!/[@]/.test(email) || !/[.]/.test(email)) {
-                    return "Please enter a valid email address"
-                }
-                return true
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your office number?',
-            name: 'office',
-            validate(office) {
-                //simple validation, as emails are super complicated to validate
-                if(/[a-zA-Z]+$/.test(office)) {
-                    return "Please only use numbers"
-                }
-                return true
-            }
-        }
-    ]).then((response) => {
-            theManager = new Manager(response.name,response.id,response.email,response.office);
-            console.log('Success!');
-            console.log(theManager);
-        }
-        
-    )
-;
 
 // TODO: init function that calls manager func
 
 // TODO: manager func that calls inquirer then calls holding menu
+managerFunc = () => {
+    let theManager = {};
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'name',
+                validate(name) {
+                    if(!/[a-zA-Z]+$/.test(name)) {
+                        return "Please only use letters";
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your emplyee ID?',
+                name: 'id',
+                validate(id) {
+                    if(/[a-zA-Z]+$/.test(id)) {
+                        return "Please only use numbers";
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
+                validate(email) {
+                    //simple validation, as emails are super complicated to validate
+                    if(!/[@]/.test(email) || !/[.]/.test(email)) {
+                        return "Please enter a valid email address"
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your office number?',
+                name: 'office',
+                validate(office) {
+                    //simple validation, as emails are super complicated to validate
+                    if(/[a-zA-Z]+$/.test(office)) {
+                        return "Please only use numbers"
+                    }
+                    return true
+                }
+            }
+        ])
+        .then((response) => {
+                theManager = new Manager(response.name,response.id,response.email,response.office);
+                console.log('Success!');
+                console.log(theManager);
+            }
+        );
+}
 
 // TODO: holding menu that display add engineer etc..., then calls the relevant function below
 
