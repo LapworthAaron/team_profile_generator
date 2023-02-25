b271b090fe7ded1d903268ffc213285bdff2a695
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-25 10:37:27 +0000

    engineer funcworking

--- starter/index.js
+++ starter/index.js
@@ -38,7 +38,7 @@ managerFunc = () => {
             },
             {
                 type: 'input',
-                message: 'What is your emplyee ID?',
+                message: 'What is your employee ID?',
                 name: 'id',
                 validate(id) {
                     if(/[a-zA-Z]+$/.test(id)) {
@@ -73,16 +73,105 @@ managerFunc = () => {
             }
         ])
         .then((response) => {
-                theManager = new Manager(response.name,response.id,response.email,response.office);
-                console.log('Success!');
-                console.log(theManager);
-            }
-        );
+            theManager = new Manager(response.name,response.id,response.email,response.office);
+            console.log('Success!');
+            console.log(theManager);
+            menuFunc();
+        });
 }
 
 // TODO: holding menu that display add engineer etc..., then calls the relevant function below
+menuFunc = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do next?',
+                name: 'option',
+                choices: [
+                    "Add an Engineer",
+                    "Add an intern",
+                    "Finish building the team"
+                ]
+            },
+        ])
+        .then((response) => {
+            if (response.option == "Add an Engineer") {
+                engineerFunc();
+            } else if (response.option == "Add an Intern") {
+                // internFunc();
+                console.log(response.option);
+            } else {
+                // render();
+                console.log(response.option);
+            }
+        }
+
+        )
+}
+
 
 // TODO: engineer func that calls inquirer for engineer then holding menu
+engineerFunc = () => {
+    let theEngineer = {};
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'name',
+                validate(name) {
+                    if(!/[a-zA-Z]+$/.test(name)) {
+                        return "Please only use letters";
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your employee ID?',
+                name: 'id',
+                validate(id) {
+                    if(/[a-zA-Z]+$/.test(id)) {
+                        return "Please only use numbers";
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
+                validate(email) {
+                    //simple validation, as emails are super complicated to validate
+                    if(!/[@]/.test(email) || !/[.]/.test(email)) {
+                        return "Please enter a valid email address"
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'github',
+                validate(github) {
+                    //simple validation, as emails are super complicated to validate
+                    if(!/[a-zA-Z]+$/.test(github)) {
+                        return "Please only use numbers"
+                    }
+                    return true
+                }
+            }
+        ])
+        .then((response) => {
+            theEngineer = new Engineer(response.name,response.id,response.email,response.github);
+            console.log('Success!');
+            console.log(theEngineer);
+            menuFunc();
+        });
+}
+
 
 // TODO: intern func that calls inquirer for intern then holding menu
 
