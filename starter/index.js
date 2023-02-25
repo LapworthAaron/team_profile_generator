cf236995dc340b66144d56bfff539e4b5033204a
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-25 10:54:06 +0000

    modularised the questions to be DRY

--- starter/index.js
+++ starter/index.js
@@ -12,66 +12,107 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// TODO: render
+// TODO: dynamic instantiation object names
 
-// TODO: init function that calls manager func
+
+// init function that calls manager func
 init = () => {
     managerFunc();
 }
 
+// reusable questions
+const nameQ = {
+    type: 'input',
+    message: 'What is your name?',
+    name: 'name',
+    validate(name) {
+        if(!/[a-zA-Z]+$/.test(name)) {
+            return "Please only use letters";
+        }
+        return true
+    }
+};
 
-// TODO: manager func that calls inquirer then calls holding menu
-// TODO: pull the 3 common questions out as a object, so they can be resused in all funcs
+const idQ = {
+    type: 'input',
+    message: 'What is your employee ID?',
+    name: 'id',
+    validate(id) {
+        if(/[a-zA-Z]+$/.test(id)) {
+            return "Please only use numbers";
+        }
+        return true
+    }
+};
+
+const emailQ = {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+    validate(email) {
+        //simple validation, as emails are super complicated to validate
+        if(!/[@]/.test(email) || !/[.]/.test(email)) {
+            return "Please enter a valid email address"
+        }
+        return true
+    }
+};
+
+const officeQ = {
+    type: 'input',
+    message: 'What is your office number?',
+    name: 'office',
+    validate(office) {
+        if(/[a-zA-Z]+$/.test(office)) {
+            return "Please only use numbers"
+        }
+        return true
+    }
+};
+
+const githubQ = {
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'github',
+    validate(github) {
+        if(!/[a-zA-Z]+$/.test(github)) {
+            return "Please only use letters"
+        }
+        return true
+    }
+}; 
+
+const schoolQ = {
+    type: 'input',
+    message: 'What school do you attend?',
+    name: 'school',
+    validate(school) {
+        if(!/[a-zA-Z]+$/.test(school)) {
+            return "Please only use letters"
+        }
+        return true
+    }
+}; 
+
+const menuQ = {
+    type: 'list',
+    message: 'What would you like to do next?',
+    name: 'option',
+    choices: [
+        "Add an Engineer",
+        "Add an intern",
+        "Finish building the team"
+    ]
+};
+
+
+// manager func that calls inquirer then calls holding menu
 managerFunc = () => {
     let theManager = {};
 
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is your name?',
-                name: 'name',
-                validate(name) {
-                    if(!/[a-zA-Z]+$/.test(name)) {
-                        return "Please only use letters";
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your employee ID?',
-                name: 'id',
-                validate(id) {
-                    if(/[a-zA-Z]+$/.test(id)) {
-                        return "Please only use numbers";
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your email address?',
-                name: 'email',
-                validate(email) {
-                    //simple validation, as emails are super complicated to validate
-                    if(!/[@]/.test(email) || !/[.]/.test(email)) {
-                        return "Please enter a valid email address"
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your office number?',
-                name: 'office',
-                validate(office) {
-                    if(/[a-zA-Z]+$/.test(office)) {
-                        return "Please only use numbers"
-                    }
-                    return true
-                }
-            }
-        ])
+        .prompt([nameQ, idQ, emailQ, officeQ])
         .then((response) => {
             theManager = new Manager(response.name,response.id,response.email,response.office);
             console.log('Success!');
@@ -83,18 +124,7 @@ managerFunc = () => {
 // TODO: holding menu that display add engineer etc..., then calls the relevant function below
 menuFunc = () => {
     inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'What would you like to do next?',
-                name: 'option',
-                choices: [
-                    "Add an Engineer",
-                    "Add an intern",
-                    "Finish building the team"
-                ]
-            },
-        ])
+        .prompt([menuQ])
         .then((response) => {
             if (response.option == "Add an Engineer") {
                 engineerFunc();
@@ -111,58 +141,12 @@ menuFunc = () => {
 }
 
 
-// TODO: engineer func that calls inquirer for engineer then holding menu
+// engineer func that calls inquirer for engineer then holding menu
 engineerFunc = () => {
     let theEngineer = {};
 
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is your name?',
-                name: 'name',
-                validate(name) {
-                    if(!/[a-zA-Z]+$/.test(name)) {
-                        return "Please only use letters";
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your employee ID?',
-                name: 'id',
-                validate(id) {
-                    if(/[a-zA-Z]+$/.test(id)) {
-                        return "Please only use numbers";
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your email address?',
-                name: 'email',
-                validate(email) {
-                    //simple validation, as emails are super complicated to validate
-                    if(!/[@]/.test(email) || !/[.]/.test(email)) {
-                        return "Please enter a valid email address"
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your github username?',
-                name: 'github',
-                validate(github) {
-                    if(!/[a-zA-Z]+$/.test(github)) {
-                        return "Please only use numbers"
-                    }
-                    return true
-                }
-            }
-        ])
+        .prompt([nameQ, idQ, emailQ, githubQ])
         .then((response) => {
             theEngineer = new Engineer(response.name,response.id,response.email,response.github);
             console.log('Success!');
@@ -172,58 +156,12 @@ engineerFunc = () => {
 }
 
 
-// TODO: intern func that calls inquirer for intern then holding menu
+// intern func that calls inquirer for intern then holding menu
 internFunc = () => {
     let theIntern = {};
 
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'What is your name?',
-                name: 'name',
-                validate(name) {
-                    if(!/[a-zA-Z]+$/.test(name)) {
-                        return "Please only use letters";
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your employee ID?',
-                name: 'id',
-                validate(id) {
-                    if(/[a-zA-Z]+$/.test(id)) {
-                        return "Please only use numbers";
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What is your email address?',
-                name: 'email',
-                validate(email) {
-                    //simple validation, as emails are super complicated to validate
-                    if(!/[@]/.test(email) || !/[.]/.test(email)) {
-                        return "Please enter a valid email address"
-                    }
-                    return true
-                }
-            },
-            {
-                type: 'input',
-                message: 'What school do you attend?',
-                name: 'school',
-                validate(school) {
-                    if(!/[a-zA-Z]+$/.test(school)) {
-                        return "Please only use numbers"
-                    }
-                    return true
-                }
-            }
-        ])
+        .prompt([nameQ, idQ, emailQ, schoolQ])
         .then((response) => {
             theIntern = new Intern(response.name,response.id,response.email,response.school);
             console.log('Success!');
@@ -231,6 +169,7 @@ internFunc = () => {
             menuFunc();
         });
 }
+
 // TODO: render func that calls the render jazz
 
 
